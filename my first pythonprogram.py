ed17d1140816d1b3a4df41d6d75bfa6f956f9dda
ayushschavan08-art <ayushschavan08@gmail.com>
2026-08-21 10:26:23 +0530

    Update my first pythonprogram.py

--- my first pythonprogram.py
+++ my first pythonprogram.py
@@ -1,1 +1,16 @@
+second project=dice rolling game
+import random 
 
+while True:
+    choice=input(str('Roll the dice?(y/n)=    ')).lower()
+    break        
+    
+if choice =='y':
+    die1=random.randint(1,6)
+    die2=random.randint(1,6)
+    print(f'({die1},{die2})')
+elif choice=='n':
+    print('thanks for playing!')    
+
+else:        
+        print('invalid choice!')
